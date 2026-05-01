5607bbf1a2c69abfff336558a7005c7775ab3d89
Md. Iqbal Hasan <mdiqbalhasan.dev@gmail.com>
2026-05-01 10:13:12 +0600

    Add files via upload

---
+++ 71A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    while (n--) {
+        char word[100];
+        scanf("%s", word);
+        int len = strlen(word);
+        
+        if (len > 10) {
+            printf("%c%d%c\n", word[0], len - 2, word[len - 1]);
+        } else {
+            printf("%s\n", word);
+        }
+    }
+    
+    return 0;
+}
